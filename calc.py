1b02a171cee610977bbc2c98a7149d8254922686
ossamahmad <ossamahmad@gmail.com>
2023-10-14 20:52:51 +0300

    main calculator code

---
+++ calc.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        return x / y
